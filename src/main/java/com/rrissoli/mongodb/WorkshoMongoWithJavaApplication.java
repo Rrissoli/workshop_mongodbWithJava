1434f7d67ff953b08090b5f0b39c60392d68e59b
Rrissoli <raphels123@gmail.com>
2023-03-27 19:36:42 -0300

    first commit

---
+++ src/main/java/com/rrissoli/mongodb/WorkshoMongoWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rrissoli.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoMongoWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoMongoWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrissoli/mongodb/WorkshoMongoWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrissoli.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoMongoWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
