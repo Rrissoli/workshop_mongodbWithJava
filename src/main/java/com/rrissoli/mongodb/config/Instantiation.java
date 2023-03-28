c220a3632ab0411895cd62e1283872f2b2c65617
Rrissoli <raphels123@gmail.com>
2023-03-28 20:31:36 -0300

    User DTO addicted

---
+++ src/main/java/com/rrissoli/mongodb/config/Instantiation.java
@@ -0,0 +1,25 @@
+package com.rrissoli.mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.rrissoli.mongodb.domain.User;
+import com.rrissoli.mongodb.repository.UserRepository;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
--- src/main/java/com/rrissoli/mongodb/resources/UserResource.java
+++ src/main/java/com/rrissoli/mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.rrissoli.mongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rrissoli.mongodb.domain.User;
+import com.rrissoli.mongodb.dto.UserDTO;
 import com.rrissoli.mongodb.services.UserService;
 
 @RestController
@@ -18,8 +20,9 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	@GetMapping
-	public ResponseEntity<List<User>>  findAll(){
+	public ResponseEntity<List<UserDTO>>  findAll(){
 		List<User>list  = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List <UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
